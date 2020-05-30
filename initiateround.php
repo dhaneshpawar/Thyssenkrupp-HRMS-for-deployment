bcb8dcab4cb9ddab52ce8efd4ccff3d5e3059252
Atharva Deshpande <34884015+AD1310@users.noreply.github.com>
2020-05-30 20:48:05 +0530

    removed button for prf

--- initiateround.php
+++ initiateround.php
@@ -253,7 +253,7 @@ $(document).ready(function(){
           console.log(appended2)
           var s1='<tr id="'+appended+'row">'
           var s2='<td>'
-          var s3='<p class="btn waves-effect blue darken-1" >'+appended+'</p></td><td>'
+          var s3='<b >'+appended+'</b></td><td>'
           var s4='<button class="waves-effect green  btn"  id="'+appended2+'" onclick="createnextround(this.id)">Initiate Round</button></td></tr>'
           var str=s1+s2+s3+s4
            $('#addtr').append(str)
